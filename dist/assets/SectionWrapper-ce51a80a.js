37026155379067eedd989d81f6e5b1f4ea99df8a
Marc Joyce <marcjoyce90@gmail.com>
2023-03-26 22:56:25 +0100

    Adding dist

---
+++ dist/assets/SectionWrapper-ce51a80a.js
@@ -0,0 +1,2 @@
+import{j as e,a as s}from"./index-2b9987de.js";import{s as i}from"./styles-bea25d13.js";import{m as t}from"./motion-866f5dc8.js";const n=(o,a)=>function(){return e(t.section,{initial:"show",whileInView:"show",viewport:{once:!0,amount:"some"},className:`${i.padding} max-w-7xl mx-auto relative z-0`,children:[s("span",{className:"hash-span",id:a,children:" "}),s(o,{})]})},l=n;export{l as S};
+//# sourceMappingURL=SectionWrapper-ce51a80a.js.map
---
+++ dist/assets/github-1dad9546.js
@@ -0,0 +1,2 @@
+const s="/assets/github-3632843a.webp";export{s as g};
+//# sourceMappingURL=github-1dad9546.js.map
