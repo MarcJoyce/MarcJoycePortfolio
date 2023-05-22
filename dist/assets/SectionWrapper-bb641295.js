325e68abd3c4d3fd04f20e5a32a893a7e4c8a3e4
Marc Joyce <marcjoyce90@gmail.com>
2023-05-22 20:56:34 +0100

    Adding dist

---
+++ dist/assets/SectionWrapper-bb641295.js
@@ -0,0 +1,2 @@
+import{j as e,a as s}from"./index-37cee617.js";import{s as i}from"./styles-bea25d13.js";import{m as t}from"./motion-bf0d207a.js";const n=(o,a)=>function(){return e(t.section,{initial:"show",whileInView:"show",viewport:{once:!0,amount:"some"},className:`${i.padding} max-w-7xl mx-auto relative z-0`,children:[s("span",{className:"hash-span",id:a,children:" "}),s(o,{})]})},l=n;export{l as S};
+//# sourceMappingURL=SectionWrapper-bb641295.js.map
