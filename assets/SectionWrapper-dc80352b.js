b5f154399e413b500c958fb0074ba37e8379832a
Marc Joyce <marcjoyce90@gmail.com>
2023-07-19 20:40:13 +0100

    Adding dist

--- assets/SectionWrapper-dc80352b.js
+++
@@ -1,2 +0,0 @@
-import{j as e,a as s}from"./index-74821668.js";import{s as i}from"./styles-bea25d13.js";import{m as t}from"./motion-c2ec759b.js";const n=(o,a)=>function(){return e(t.section,{initial:"show",whileInView:"show",viewport:{once:!0,amount:"some"},className:`${i.padding} max-w-7xl mx-auto relative z-0`,children:[s("span",{className:"hash-span",id:a,children:" "}),s(o,{})]})},l=n;export{l as S};
-//# sourceMappingURL=SectionWrapper-dc80352b.js.map
