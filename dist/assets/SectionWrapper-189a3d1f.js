edb5fca951b85bceb5ec220e4e9e3f683509a7db
Marc Joyce <marcjoyce90@gmail.com>
2024-01-10 22:57:07 +0000

    Adding dist

--- dist/assets/SectionWrapper-189a3d1f.js
+++
@@ -1,2 +0,0 @@
-import{j as e,a as s}from"./index-1f79ed04.js";import{s as i}from"./styles-bea25d13.js";import{m as t}from"./motion-33878e9a.js";const n=(o,a)=>function(){return e(t.section,{initial:"show",whileInView:"show",viewport:{once:!0,amount:"some"},className:`${i.padding} max-w-7xl mx-auto relative z-0`,children:[s("span",{className:"hash-span",id:a,children:" "}),s(o,{})]})},l=n;export{l as S};
-//# sourceMappingURL=SectionWrapper-189a3d1f.js.map
---
+++ dist/assets/SectionWrapper-3aac89ad.js
@@ -0,0 +1,2 @@
+import{j as e,a as s}from"./index-665e387e.js";import{s as i}from"./styles-bea25d13.js";import{m as t}from"./motion-32448cbc.js";const c=(a,o)=>function(){return e(t.section,{initial:"show",whileInView:"show",viewport:{once:!0,amount:"some"},className:`${i.padding} max-w-7xl mx-auto relative z-0`,children:[s("span",{className:"hash-span",id:o,children:" "}),s(a,{})]})};export{c as S};
+//# sourceMappingURL=SectionWrapper-3aac89ad.js.map
